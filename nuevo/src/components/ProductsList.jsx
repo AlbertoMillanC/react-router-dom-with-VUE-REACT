434fa6c38032d504aafb26cf6e8ab6bae2d11a82
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-12-15 21:29:09 -0500

    add link to product.id

--- nuevo/src/components/ProductsList.jsx
+++ nuevo/src/components/ProductsList.jsx
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom"
 function ProductsList({products}) {
   return (
     <ul>
-    {products.map(product =>{
-        return (
-        <li key={product.id}>
-            <strong>{product.title}</strong>{" "}
-           <Link to ={'/products/${product.id}'}>more details </Link>
-        </li>
-        )
+        {products.map(product =>{
+            return (
+                <li key={product.id}>
+                    <strong>{product.title}</strong>{" "}
+                    <Link to={`/products/${product.id}`}>[more details+] </Link>
+
+                </li>
+            )
 
     }
         
