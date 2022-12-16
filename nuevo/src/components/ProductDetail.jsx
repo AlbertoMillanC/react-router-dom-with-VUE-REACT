420789f4b2093bced81def52d341301931ba6645
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-12-15 21:55:40 -0500

    add product.category, product.description, product.title and img of product

--- nuevo/src/components/ProductDetail.jsx
+++ nuevo/src/components/ProductDetail.jsx
@@ -1,13 +1,32 @@
+import Axios  from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 
 function ProductDetail() {
+  const [product, setProduct] = useState (null);
+
   const {productId} = useParams();
- 
+
+  useEffect(() =>{ 
+     Axios.get(`https://dummyjson.com/products/${productId}`)
+        .then(res => setProduct(res.data))
+  }, [productId]);
+  
   return (
-    <>
-    <h4>Product Detail: {productId}  </h4>
+    <>  +
+        {product && <em> Loading...</em>}
+        {product && (
+            <>
+                <h4>Product Detail: {product.title}  </h4>
+                <img src={product.thumbnail} alt={product.title}
+                width="200" />
+                <p>Category: {product.category}</p>
+                <p>Description: {product.description}</p>
+            </>
+    )}
     </>
+    
   )
 }
 
