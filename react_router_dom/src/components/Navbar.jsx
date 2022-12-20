965a686f7798e6d8d214edca0beec38e222cf91b
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-12-19 21:07:33 -0500

    add jsconfig.json CUIDADO*****

--- react_router_dom/src/components/Navbar.jsx
+++ react_router_dom/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { useState,  } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import logo from '../media/logo192.png';
-import App from "../styles/App.css";
 import "../index.css";
 
 
